cd548d703d351580654a0f466aac493d916b8396
Ujjwal <ujjwalkandera23@tutamail.com>
2025-07-16 19:36:23 +0530

    setup project structure

---
+++ src/main.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <cstdint>
+
+// Type aliases for the gameboy's data types for easier management
+using BYTE = uint8_t;
+using SIGNED_BYTE = int8_t;
+using WORD = uint16_t;
+using SIGNED_WORD = int16_t;
